a7034cbbf5b5f44418f0e9aebde23cfae075eb4e
Sebastien Langoureaux <sebastien.langoureaux@sihm.fr>
2018-05-29 13:21:47 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,131 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+provider "azurerm" {}
+
+
+locals {
+  virtual_machine_fqdn = "${local.virtual_machine_name}.${var.active_directory_domain}"
+  custom_data_params   = "Param($RemoteHostName = \"${local.virtual_machine_fqdn}\", $ComputerName = \"${local.virtual_machine_name}\")"
+  custom_data_content  = "${local.custom_data_params} ${file("${path.module}/files/winrm.ps1")}"
+  import_command       = "Import-Module ADDSDeployment"
+  password_command     = "$password = ConvertTo-SecureString ${var.admin_password} -AsPlainText -Force"
+  install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
+  configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName ${var.active_directory_domain} -DomainNetbiosName ${var.active_directory_netbios_name} -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
+  shutdown_command     = "shutdown -r -t 10"
+  exit_code_hack       = "exit 0"
+  powershell_command   = "${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                         = "vm_${var.name}_public_ip"
+  location                     = "${var.location}"
+  resource_group_name          = "${var.rg_name}"
+  public_ip_address_allocation = "dynamic"
+  domain_name_label            = "${var.subdomain}-${var.name}"
+  tags = "${var.tags}"
+}
+
+resource "azurerm_network_interface" "interface" {
+  name                    = "vm_${var.name}_interface"
+  location                = "${var.location}"
+  resource_group_name     = "${var.rg_name}"
+  internal_dns_name_label = "${var.name}"
+  ip_configuration {
+    name                          = "primary"
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "static"
+    private_ip_address            = "${cidrhost(var.network, count.index + var.start_ip)}"
+    public_ip_address_id          = "${azurerm_public_ip.public_ip.id}"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                          = "vm_${var.name}${count.index}"
+  location                      = "${var.location}"
+  resource_group_name           = "${var.rg_name}"
+  network_interface_ids         = ["${azurerm_network_interface.interface.id}"]
+  vm_size                       = "${var.size}"
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "vm_${var.name}_disk_system"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+    disk_size_gb      = "${var.os_disk_size}"
+  }
+
+  os_profile {
+    computer_name  = "${var.name}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+    custom_data    = "${local.custom_data_content}"
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent        = true
+    enable_automatic_upgrades = true
+
+    additional_unattend_config {
+      pass         = "oobeSystem"
+      component    = "Microsoft-Windows-Shell-Setup"
+      setting_name = "AutoLogon"
+      content      = "<AutoLogon><Password><Value>${var.admin_password}</Value></Password><Enabled>true</Enabled><LogonCount>1</LogonCount><Username>${var.admin_username}</Username></AutoLogon>"
+    }
+
+    # Unattend config is to enable basic auth in WinRM, required for the provisioner stage.
+    additional_unattend_config {
+      pass         = "oobeSystem"
+      component    = "Microsoft-Windows-Shell-Setup"
+      setting_name = "FirstLogonCommands"
+      content      = "${file("${path.module}/files/FirstLogonCommands.xml")}"
+    }
+  }
+}
+
+
+// NOTE: we **highly recommend** not using this configuration for your Production Environment
+// this provisions a single node configuration with no redundancy.
+resource "azurerm_virtual_machine_extension" "create-active-directory-forest" {
+  name                 = "create-active-directory-forest"
+  location             = "${azurerm_virtual_machine.domain-controller.location}"
+  resource_group_name  = "${var.rg_name}"
+  virtual_machine_name = "${azurerm_virtual_machine.domain-controller.name}"
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\""
+    }
+SETTINGS
+}
+
+resource "azurerm_virtual_machine_extension" "run_extra_powershell" {
+  name                 = "run_extra_powershell"
+  location             = "${azurerm_virtual_machine.domain-controller.location}"
+  resource_group_name  = "${var.rg_name}"
+  virtual_machine_name = "${azurerm_virtual_machine.domain-controller.name}"
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell.exe -command \"${var.extra_powershell}\""
+    }
+SETTINGS
+
+  depends_on = ["azurerm_virtual_machine_extension.create-active-directory-forest", "null_resource.copy_extra_powershell"]
+}
---
+++ variables.tf
@@ -0,0 +1,65 @@
+variable "location" {
+    description = "The Azure Region in which the Resource Group exists"
+}
+
+variable "rg_name" {
+    description = "The name of the Resource Group where VM resources will be created"
+}
+
+variable "subnet_id" {
+    description = "The Subnet ID which the VM's NIC should be created in"
+}
+
+variable "name" {
+    description = "The Prefix used for the VM's resources and for the VM hostname"
+}
+
+variable "size" {
+    description = "The VM model"
+}
+
+variable "network" {
+    description = "The network to allocate static IP"
+}
+
+variable "start_ip" {
+    description = "Where the IP allocation begin for static private IP"
+}
+
+variable "subdomain" {
+    description = "The subdomain to use for public FQDN"
+}
+
+variable "os_disk_size" {
+    description = "The OS disk size in Gb"
+    default = "30"
+}
+
+
+variable "active_directory_domain" {
+  description = "The name of the Active Directory domain, for example `consoto.local`"
+}
+
+variable "active_directory_netbios_name" {
+  description = "The netbios name of the Active Directory domain, for example `consoto`"
+}
+
+variable "admin_username" {
+  description = "The username associated with the local administrator account on the virtual machine"
+}
+
+variable "admin_password" {
+  description = "The password associated with the local administrator account on the virtual machine"
+}
+
+variable "extra_powershell" {
+  description = "Launch extra powershell script after provisionning AD like users / groups creation"
+}
+
+variable "tags" {
+    description = "List of tags should be associated on all ressoruces"
+    type = "map"
+    default = {
+        module = "module-azure-ad"
+    }
+}
